3a5231de46042993de97f7c77f2d55852daf4667
Ye Wang <wangyegp@gmail.com>
2022-11-10 22:09:52 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import {BakeryItem} from "./BakeryItem.js"
 import bakeryData from "./assets/bakery-data.json";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
@@ -11,18 +12,28 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [counter, setCounter] = useState(0);
+  const [allItems, setAllItems] = useState([]);
+
+  function handleClick(item) {
+    setCounter(counter + item.price);
+    setAllItems(allItems.concat(item.name));
+  }
+
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem key={index} item={item} addCounter={handleClick}/> // replace with BakeryItem component
       ))}
 
       <div>
         <h2>Cart</h2>
         {/* TODO: render a list of items in the cart */}
+          <p>Cart Items: {allItems.join(", ")}</p>
+          <p>Total: {counter}</p>
       </div>
     </div>
   );
